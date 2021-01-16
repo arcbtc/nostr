7e89b21d67be73262e391efa0e0877e2c73d1d92
benarc <ben@arc.wales>
2021-01-16 11:51:36 +0000

    Deleted senDM from messages

--- src/pages/PageMessages.vue
+++ src/pages/PageMessages.vue
@@ -92,45 +92,6 @@ export default {
 	mixins: [myHelpers],
 
 	methods: {
-		async sendDM(message, pubkey) {
-			const pool = relayPool();
-
-			pool.setPrivateKey(this.$q.localStorage.getItem("privkey")); // optional
-
-			pool.addRelay("wss://nostr-relay.bigsun.xyz", {
-				read: true,
-				write: true,
-			});
-
-			pool.onEvent((event, context, relay) => {
-				console.log(
-					`got a relay with context ${context} from ${relay.url} which is already validated.`,
-					event
-				);
-			});
-			console.log(pool);
-			// subscribing to users and requesting specific users or events:
-			pool.subKey(
-				"f9e10c85f4baf021b88b4d19534ac25b81a5ca2a1fc99c8fe2e3bb40e85f9d10"
-			);
-
-			await pool.reqFeed();
-			//pool.reqEvent({id: '<hex>'})
-			//pool.reqKey({key: '<hex>'})
-			// upon request the events will be received on .onEvent above
-
-			// publishing events:
-			//pool.publish(<event object>)
-			// it will be signed automatically with the key supplied above
-			// or pass an already signed event to bypass this
-
-			// subscribing to a new relay
-			//pool.addRelay('<url>')
-			// will automatically subscribe to the all the keys called with .subKey above
-
-			console.log(pool);
-		},
-
 		MessageonReset() {
 			this.name = null;
 			this.age = null;

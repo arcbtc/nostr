92a04e1cc1365870046df85a21c5457dabab04bc
benarc <ben@arc.wales>
2021-01-31 23:43:16 +0000

    Cant get reply working

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,8 @@
 <template>
   <q-page>
-    <q-dialog v-model="dialogReply" position="top"> <Reply post:/> </q-dialog>
+    <q-dialog v-model="dialogReply" position="top">
+      <Reply :post="dialogReply" />
+    </q-dialog>
 
     <Publish v-if="!$store.getters.disabled" />
     <Post v-for="post in $store.state.kind1" :key="post.id" :post="post" />
